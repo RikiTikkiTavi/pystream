f4a4b7e85bdc20e24dd87f39fbca2fd7aaac652d
Yahor Paromau <egor.latvia@gmail.com>
2020-03-03 23:51:17 +0100

    Parallel stream improvements (lazy)

--- pystream/parallel_stream.py
+++ pystream/parallel_stream.py
@@ -1,13 +1,14 @@
 from functools import partial, reduce
 from itertools import chain, repeat, islice
 from multiprocessing.pool import Pool
-from os import cpu_count
+from multiprocessing import cpu_count
 from typing import Generic, TypeVar, Callable, NamedTuple, Any, List, Iterable, Iterator, Union, Generator, cast, Tuple, \
     Collection, Protocol
 
 from pystream.abstracts import abstract_collector as a_c
 from pystream.abstracts.abstract_base_stream import AbstractBaseStream
 from pystream.collectors.single_thread import to_collection
+from pystream.helpers.parallel_stream_helpers import partition_generator
 from pystream.nullable import Nullable
 from pystream.stream import Stream
 
@@ -16,12 +17,12 @@ S = TypeVar('S')
 G = TypeVar('G')
 
 
-def _map_partition(partition: Iterable[T], fun: Callable[[T], S]) -> Tuple[S, ...]:
-    return Stream(partition).map(fun).collect(to_collection(tuple))
+def _map_partition(*elements: T, fun: Callable[[T], S]) -> Tuple[S, ...]:
+    return Stream(elements).map(fun).collect(to_collection(tuple))
 
 
-def _filter_partition(partition: Iterable[T], fun: Callable[[T], bool]) -> Tuple[T, ...]:
-    return Stream(partition).filter(fun).collect(to_collection(tuple))
+def _filter_partition(*elements: T, fun: Callable[[T], bool]) -> Tuple[T, ...]:
+    return Stream(elements).filter(fun).collect(to_collection(tuple))
 
 
 class ParallelStream(Generic[T], AbstractBaseStream[T]):
@@ -29,38 +30,38 @@ class ParallelStream(Generic[T], AbstractBaseStream[T]):
     __scheduled: List[Callable]
     __pool: Pool
 
-    def __init__(self, *iterables: Iterable[T], n_workers: int = cpu_count()):
+    def __init__(self, *iterables: Iterable[T]):
         super().__init__(*iterables)
-        self.__n_workers = n_workers
         self.__scheduled = []
 
-    def __partition_generator(self, collection: Collection[T], n_elements: int) -> Generator[Tuple[T, ...], None, None]:
-        it: Iterator[T] = iter(collection)
-        while True:
-            partition: Tuple[T, ...] = tuple(islice(it, n_elements))
-            if len(partition) > 0:
-                yield partition
-            else:
-                break
-
     def __create_partitions(self, collection: Collection[T]) -> Tuple[Tuple[T, ...], ...]:
-        return *self.__partition_generator(collection, self.__calculate_n_elements(collection)),
+        return *partition_generator(collection, self.__calculate_n_elements(collection)),
 
     def __calculate_n_elements(self, collection: Collection[T]) -> int:
         n: int = len(collection) // self.__n_workers
         return n if n > 0 else 1
 
     def __map(self, fun: Callable[[T], S], collection: Collection[T]) -> Tuple[S, ...]:
-        partitions: Tuple[Tuple[T, ...], ...] = self.__create_partitions(collection)
-        return tuple(chain(*self.__pool.map(partial(_map_partition, fun=fun), partitions)))
+        # partitions: Tuple[Tuple[T, ...], ...] = self.__create_partitions(collection)
+        # noinspection Mypy
+        return tuple(chain(*(self.__pool.imap(
+            partial(_map_partition, fun=fun),
+            collection,
+            chunksize=self.__calculate_n_elements(collection)
+        ))))
 
     def map(self, fun: Callable[[T], S]) -> 'ParallelStream[S]':
         self.__scheduled.append(partial(self.__map, fun))
         return self
 
     def __filter(self, fun: Callable[[T], bool], collection: Collection[T]) -> Tuple[T, ...]:
-        partitions: Tuple[Tuple[T, ...], ...] = self.__create_partitions(collection)
-        return tuple(chain(*self.__pool.map(partial(_filter_partition, fun=fun), partitions)))
+        # partitions: Tuple[Tuple[T, ...], ...] = self.__create_partitions(collection)
+        # noinspection Mypy
+        return tuple(chain(*(self.__pool.imap(
+            partial(_filter_partition, fun=fun),
+            collection,
+            chunksize=self.__calculate_n_elements(collection)
+        ))))
 
     def filter(self, fun: Callable[[T], bool]) -> 'ParallelStream[T]':
         self.__scheduled.append(partial(self.__filter, fun))
@@ -70,5 +71,6 @@ class ParallelStream(Generic[T], AbstractBaseStream[T]):
         with Pool(processes=self.__n_workers) as self.__pool:
             return Stream(reduce(lambda seq, fun: fun(seq), self.__scheduled, tuple(self._iterable)))
 
-    def collect(self, collector: a_c.AbstractCollector[T, S]) -> S:
+    def collect(self, collector: a_c.AbstractCollector[T, S], n_workers: int = cpu_count()) -> S:
+        self.__n_workers = n_workers
         return collector.collect(self.__collect())
