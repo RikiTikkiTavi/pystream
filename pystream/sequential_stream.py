46572a31570c9297b8404bccfe9d1137dd2d6228
Yahor Paromau <egor.latvia@gmail.com>
2020-03-18 16:17:18 +0100

    Start writing docs of sequentiall stream

--- pystream/sequential_stream.py
+++ pystream/sequential_stream.py
@@ -12,12 +12,12 @@ _AT = TypeVar('_AT')
 _RT = TypeVar('_RT')
 
 
-class SequentialStream(Generic[_AT]):
-    """SequentialStream class to perform functional-style operations in an aesthetically-pleasing manner.
+class SequentialStream(Generic[_AT], Iterable[_AT]):
+    """
+    SequentialStream class to perform functional-style operations in an aesthetically-pleasing manner.
+    Performs operations sequentially.
 
-    Args:
-        *iterables (Iterable) : Source iterables for the SequentialStream object. When multiple iterables are given,
-        they will be concatenated.
+    :param `*iterables`: Source iterables for the SequentialStream object.  When multiple iterables are given, they will be concatenated.
     """
 
     __iterable: Iterable[_AT]
@@ -29,45 +29,108 @@ class SequentialStream(Generic[_AT]):
         return self.iterator()
 
     def iterator(self) -> Iterator[_AT]:
-        """Creates iterator from stream. This is terminal operation."""
+        """
+        Creates iterator from stream.
+        This is terminal operation.
+
+        :returns: Iterator over stream elements
+        """
         return iter(self.__iterable)
 
     def partition_iterator(self, partition_size: int) -> Generator[List[_AT], None, None]:
-        """Creates iterator over partitions of stream. This is terminal operation."""
+        """
+        Creates iterator over partitions of stream. This is terminal operation.
+
+        :param partition_size: Length of partition
+        :returns: Iterator over partitions of stream.
+        """
         return utils.partition_generator(self.iterator(), partition_size)
 
     def map(self, mapper: Callable[[_AT], _RT]) -> "SequentialStream[_RT]":
-        """Maps elements using the supplied function."""
+        """
+        Returns a stream consisting of the results of applying the given function to the elements of this stream.
+        This is an intermediate operation.
+
+        :param mapper: Mapper function
+        :return: Stream with mapper operation lazily applied
+        """
         return SequentialStream(map(mapper, self.__iterable))
 
     def filter(self, predicate: Callable[[_AT], bool]) -> "SequentialStream[_AT]":
-        """Filters elements using the supplied function."""
+        """
+        Returns a stream consisting of the elements of this stream that match the given predicate.
+        This is an intermediate operation.
+
+        :param predicate: Predicate to apply to each element to determine if it should be included
+        :return: The new stream
+        """
         return SequentialStream(filter(predicate, self.__iterable))
 
-    def reduce(self, start_value: _RT, reducer: Callable[[_RT, _AT], _RT]) -> _RT:
-        """Reduce using the supplied function."""
-        return reduce(reducer, self.__iterable, start_value)
+    def reduce(self, identity: _RT, accumulator: Callable[[_RT, _AT], _RT]) -> _RT:
+        """
+        Performs a reduction on the elements of this stream, using the provided identity value and an associative
+        accumulation function, and returns the reduced value.
+
+        :param identity: The identity value for the accumulating function
+        :param accumulator: Function for combining two values
+        :return: The result of the reduction
+        """
+        return reduce(accumulator, self.__iterable, identity)
 
     def for_each(self, action: Callable[[_AT], Any]) -> None:
-        """Calls the function with each element. This is a terminal operation."""
+        """
+        Performs an action for each element of this stream.
+        This is terminal operation.
+
+        :param action: An action to perform on the elements
+        """
         for i in self.__iterable:
             action(i)
 
     def any_match(self, predicate: Callable[[_AT], bool]) -> bool:
-        """Returns True if any element of the stream matches the criteria."""
+        """
+        Returns whether any elements of this stream match the provided predicate.
+        May not evaluate the predicate on all elements if not necessary for determining the result.
+        If the stream is empty then false is returned and the predicate is not evaluated.
+
+        :param predicate: A predicate to apply to elements of this stream.
+        :return: true if any elements of the stream match the provided predicate, otherwise false.
+        """
         return any(self.map(predicate))
 
     def all_match(self, predicate: Callable[[_AT], bool]) -> bool:
-        """Returns True if all elements of the stream match the criteria."""
+        """
+        Returns whether all elements of this stream match the provided predicate.
+        May not evaluate the predicate on all elements if not necessary for determining the result.
+        If the stream is empty then true is returned and the predicate is not evaluated.
+
+        :param predicate: A predicate to apply to elements of this stream.
+        :return: true if either all elements of the stream match the provided predicate or the stream is empty, otherwise false
+        """
         return all(self.map(predicate))
 
     def none_match(self, predicate: Callable[[_AT], bool]) -> bool:
-        """Returns True if no element of the stream matches the criteria."""
+        """
+        Returns whether no elements of this stream match the provided predicate. May not evaluate the predicate on
+        all elements if not necessary for determining the result. If the stream is empty then true is returned and
+        the predicate is not evaluated.
+
+        :param predicate: A predicate to apply to elements of this stream.
+        :return: true if either no elements of the stream match the provided predicate or the stream is empty, otherwise false
+        """
         return not self.any_match(predicate)
 
     def flat_map(self, mapper: Callable[[_AT], "SequentialStream[_RT]"]) -> "SequentialStream[_RT]":
         """
-        When iterating over lists, flattens the stream by concatenating all lists using mapper function.
+        Returns a stream consisting of the results of replacing each element of this stream with the contents of a
+        mapped stream produced by applying the provided mapping function to each element.
+
+        **API Note**:
+        The flatMap() operation has the effect of applying a one-to-many transformation to the elements of the stream,
+        and then flattening the resulting elements into a new stream.
+
+        :param mapper: Function to apply to each element which produces a stream of new values.
+        :return: The new stream
         """
         return SequentialStream(chain.from_iterable(map(mapper, self.__iterable)))
 
@@ -99,20 +162,29 @@ class SequentialStream(Generic[_AT]):
         return max(self.__iterable)
 
     def limit(self, number: int) -> "SequentialStream[_AT]":
-        """Limit the stream to a specific number of items."""
+        """
+        Limit the stream to a specific number of items.
+        """
         return SequentialStream(islice(self.__iterable, number))
 
     def find_first(self) -> nullable.Nullable[_AT]:
         """
-        Returns a nullable containing the first element of the stream.
-        If the stream is empty, returns an empty nullable.
+        Returns an Nullable describing the first element of this stream, or an empty Nullable if the stream is empty.
+
+        :return: An Nullable describing the first element of this stream, or an empty Nullable if the stream is empty
         """
         return nullable.Nullable(next(self.__iterable, None))
 
-    def find_any(self) -> 'nullable.Nullable[_AT]':
-        return self.find_first()
-
     def peek(self, action: Callable[[_AT], Any]) -> 'SequentialStream[_AT]':
+        """
+        Returns a stream consisting of the elements of this stream, additionally performing the provided action on each
+        element as elements are consumed from the resulting stream.
+        This is an intermediate operation.
+
+        :param action: An action to perform on the elements as they are consumed from the stream
+        :return: the new stream
+        """
+
         def with_action(x):
             action(x)
             return x
